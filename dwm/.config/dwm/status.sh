6fb5ca453d2ee47c5df6cc3bc3686dbf1890e58a
michacode <micbr@protonmail.com>
2020-09-02 23:44:54 +0000

    Patch: vanitygaps, dull font

--- dwm/.config/dwm/status.sh
+++ dwm/.config/dwm/status.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-#bat=$( cat ~/.config/dwm/blocks/bat.txt )
 bat=$( cat < bat )
 tid=$( ~/.config/dwm/blocks/tid.sh )
-#lav lys knap
 lys=$( ~/.config/dwm/blocks/lys.sh )
 vol=$( ~/.config/dwm/blocks/volume.sh )
 
