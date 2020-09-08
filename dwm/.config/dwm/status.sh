d5738afa06d8516951f9e009a93abc0c0d0c221f
michacode <micbr@protonmail.com>
2020-09-08 00:02:58 +0000

    rykket lidt rundt

--- dwm/.config/dwm/status.sh
+++ dwm/.config/dwm/status.sh
@@ -6,6 +6,5 @@ lys=$( ~/.config/dwm/blocks/lys.sh )
 bat=$( cat < bat )
 tid=$( ~/.config/dwm/blocks/tid.sh )
 net=$(  ~/.config/dwm/blocks/net.sh )
-#ikon_btc=$(echo -e '\xEF\x8D\xB9')
-#ikon_tid=$(echo -e )
-xsetroot -name " $crypto  |  $vol  |  lys $lys%  |  net $net  |  bat. $bat  |  $tid"
+
+xsetroot -name " $crypto  |  lys $lys%  |  $vol  |  bat. $bat  |  net $net   |  $tid "
