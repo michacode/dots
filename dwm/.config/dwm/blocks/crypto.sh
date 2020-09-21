47dadc34de2bc5b4256b29e673707523a843ffa4
michacode <micbr@protonmail.com>
2020-09-21 10:25:34 +0000

    21.9.20: btc og eth

--- dwm/.config/dwm/blocks/crypto.sh
+++ dwm/.config/dwm/blocks/crypto.sh
@@ -3,7 +3,9 @@
 [[ -e /tmp/crypto ]] && rm /tmp/crypto
 
 while true; do
-   curl -s usd.rate.sx/?qT:BTC | awk '/BTC/{print "$BTC: "$6," ·  24t /"$8,"  ·  1t /"$10;}' | awk 'NR==2' >/tmp/crypto
+   curl -s usd.rate.sx/?qT:BTC | awk '/BTC/{print "$BTC: "$6," ·  24t /"$8;}' | awk 'NR==2' >/tmp/crypto
+    curl -s usd.rate.sx/?gT:ETH  | awk '/ETH/{print "$ETH: "$6,"· 24t /"$8;}' | awk 'NR==1' >>/tmp/crypto
+
    ~/.config/dwm/status.sh
   sleep 300
 done &
--- dwm/.config/dwm/status.sh
+++ dwm/.config/dwm/status.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-crypto=$( cat </tmp/crypto )
+crypto_btc=$( cat </tmp/crypto | awk 'NR==1' )
+crypto_eth=$( cat </tmp/crypto | awk 'NR==2' )
 vol=$( ~/.config/dwm/blocks/volume.sh )
 lys=$( ~/.config/dwm/blocks/lys.sh )
 bat=$( cat </tmp/bat  )
 tid=$( ~/.config/dwm/blocks/tid.sh )
 net=$(  ~/.config/dwm/blocks/net.sh )
 
-xsetroot -name " $crypto  |  LYS: $lys%  |  $vol  |  BAT.: $bat  |  NET: $net   |  $tid "
+xsetroot -name " $crypto_btc  ·  $crypto_eth  |  LYS: $lys%  |  $vol  |  BAT.: $bat  |  NET: $net   |  $tid "
