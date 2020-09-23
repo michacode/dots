4dbe8af78de2147151605aa121dafa6ce4ac62e3
michacode <micbr@protonmail.com>
2020-09-23 23:18:33 +0000

    Crypto status opdateres kun hvis der er forbindelse til WWW

--- dwm/.config/dwm/blocks/crypto.sh
+++ dwm/.config/dwm/blocks/crypto.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-[[ -e "/tmp/crypto.sh" ]] && rm /tmp/crypto
+
+if ping -q -w 1 -c 1 8.8.8.8 > /dev/null; then
+[[ -e /tmp/crypto ]] && rm /tmp/crypto
 while true; do
   curl -s usd.rate.sx/?qT:BTC | awk '/BTC/{print "$BTC: "$6,"· 24t /"$8;}' | awk 'NR==2' >/tmp/crypto
   curl -s usd.rate.sx/?gT:ETH  | awk '/ETH/{print "$ETH: "$6,"· 24t /"$8;}' | awk 'NR==1' >>/tmp/crypto
@@ -7,3 +9,5 @@ while true; do
   ~/.config/dwm/status.sh
   sleep 300
 done &
+else :
+fi
