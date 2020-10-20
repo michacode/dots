0fca275cd3c1a0b6cdc348c849717b33694252d7
michacode <micbr@protonmail.com>
2020-10-20 07:28:25 +0000

    Dwm fibonacci ramme og skift til zsh
    Fibonacci ramme, tilføjet dwm/fibonacci.c; Skiftet skal til zsh; opdateret dot-skript

--- dwm/dwm_source/dwm/fibonacci.c
+++ dwm/dwm_source/dwm/fibonacci.c
@@ -1,66 +1,74 @@
-void
-fibonacci(Monitor *mon, int s) {
-	unsigned int i, n, nx, ny, nw, nh;
-	Client *c;
+static void
+fibonacci(Monitor *m, int s)
+{
+ unsigned int i, n;
+ int nx, ny, nw, nh;
+ Client *c;
 
-	for(n = 0, c = nexttiled(mon->clients); c; c = nexttiled(c->next), n++);
-	if(n == 0)
-		return;
-	
-	nx = mon->wx;
-	ny = 0;
-	nw = mon->ww;
-	nh = mon->wh;
-	
-	for(i = 0, c = nexttiled(mon->clients); c; c = nexttiled(c->next)) {
-		if((i % 2 && nh / 2 > 2 * c->bw)
-		   || (!(i % 2) && nw / 2 > 2 * c->bw)) {
-			if(i < n - 1) {
-				if(i % 2)
-					nh /= 2;
-				else
-					nw /= 2;
-				if((i % 4) == 2 && !s)
-					nx += nw;
-				else if((i % 4) == 3 && !s)
-					ny += nh;
-			}
-			if((i % 4) == 0) {
-				if(s)
-					ny += nh;
-				else
-					ny -= nh;
-			}
-			else if((i % 4) == 1)
-				nx += nw;
-			else if((i % 4) == 2)
-				ny += nh;
-			else if((i % 4) == 3) {
-				if(s)
-					nx += nw;
-				else
-					nx -= nw;
-			}
-			if(i == 0)
-			{
-				if(n != 1)
-					nw = mon->ww * mon->mfact;
-				ny = mon->wy;
-			}
-			else if(i == 1)
-				nw = mon->ww - nw;
-			i++;
-		}
-		resize(c, nx, ny, nw - 2 * c->bw, nh - 2 * c->bw, False);
-	}
+ for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+ if (n == 0)
+   return;
+ if (n == 1) {
+   c = nexttiled(m->clients);
+   resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+   return;
+ }
+
+ nx = m->wx + m->gappih;
+ ny = m->gappih;
+ nw = m->ww - 2*m->gappih;
+ nh = m->wh - 2*m->gappih;
+
+ for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
+   if ((i % 2 && nh / 2 > 2*c->bw)
+      || (!(i % 2) && nw / 2 > 2*c->bw)) {
+     if (i < n - 1) {
+       if (i % 2)
+         nh = (nh - m->gappih) / 2;
+       else
+         nw = (nw - m->gappih) / 2;
+
+       if ((i % 4) == 2 && !s)
+         nx += nw + m->gappih;
+       else if ((i % 4) == 3 && !s)
+         ny += nh + m->gappih;
+     }
+     if ((i % 4) == 0) {
+       if (s)
+         ny += nh + m->gappih;
+       else
+         ny -= nh + m->gappih;
+     }
+     else if ((i % 4) == 1)
+       nx += nw + m->gappih;
+     else if ((i % 4) == 2)
+       ny += nh + m->gappih;
+     else if ((i % 4) == 3) {
+       if (s)
+         nx += nw + m->gappih;
+       else
+         nx -= nw + m->gappih;
+     }
+     if (i == 0) {
+       if (n != 1)
+         nw = (m->ww - 2*m->gappih - m->gappih) * m->mfact;
+       ny = m->wy + m->gappih;
+     }
+     else if (i == 1)
+       nw = m->ww - nw - m->gappih - 2*m->gappih;
+     i++;
+   }
+
+   resize(c, nx, ny, nw - (2*c->bw), nh - (2*c->bw), False);
+ }
 }
 
 void
 dwindle(Monitor *mon) {
-	fibonacci(mon, 1);
+ fibonacci(mon, 1);
 }
 
 void
 spiral(Monitor *mon) {
-	fibonacci(mon, 0);
+ fibonacci(mon, 0);
 }
