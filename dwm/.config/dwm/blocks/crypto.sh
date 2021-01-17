26c8fe5faccd0793c78e8daa9db8d796d079487c
michacode <micbr@protonmail.com>
2021-01-17 00:24:08 +0000

    17.01.2021
    .alias x-funktion

--- dwm/.config/dwm/blocks/crypto.sh
+++ dwm/.config/dwm/blocks/crypto.sh
@@ -4,7 +4,7 @@
 
 while true; do
     curl -s usd.rate.sx/?qT:BTC | awk '/BTC/{print "$BTC: "$6,"· 24t /"$8;}' | awk 'NR==2' >/tmp/crypto
-    curl -s usd.rate.sx/?gT:DOT  | awk '/DOT/{print "$DOT: "$6,"· 24t /"$8;}' | awk 'NR==1' >>/tmp/crypto
+    curl -s usd.rate.sx/?qT:DOT  | awk '/DOT/{print "$DOT: "$6,"· 24t /"$8;}' | awk 'NR==1' >>/tmp/crypto
     ~/.config/dwm/status.sh
     sleep 300
 done &
